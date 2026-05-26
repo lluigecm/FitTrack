b6f4d69fca185fb6bc98dbad728e1b04d8f35c00
lluige <luige030301@gmail.com>
2026-05-25 23:56:46 -0300

    ReactiveForms

--- src/app/pages/treino-form/treino-form.ts
+++ src/app/pages/treino-form/treino-form.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-treino-form',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './treino-form.html',
-  styleUrl: './treino-form.scss',
+  styleUrl: './treino-form.scss'
 })
-export class TreinoForm {}
+export class TreinoForm {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      nome: [ null, [Validators.required, Validators.minLength(3)]],
+      data: [ null, Validators.required],
+      exercicio: [ null, Validators.required],
+      series: [ null, [Validators.required, Validators.min(1)]],
+      repeticoes: [ null, [Validators.required, Validators.min(1)]],
+      carga: [ null, [Validators.required, Validators.min(0)]],
+    });
+  }
+
+  salvar() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+  }
+}
