a7a8154de04685c36d63ef6b3b411da481321a16
lluige <luige030301@gmail.com>
2026-06-29 23:11:40 -0300

    Protege rotas internas com guard de autenticacao (sessionStorage)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,15 +5,16 @@ import { Treinos } from './pages/treinos/treinos';
 import { TreinoDetalhe } from './pages/treino-detalhe/treino-detalhe';
 import { Exercicios } from './pages/exercicios/exercicios';
 import { TreinoForm } from './pages/treino-form/treino-form';
+import { authGuard } from './services/auth.guard';
 
 
 export const routes: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},
     { path: 'login', component: Login },
-    { path: 'dashboard', component: Dashboard },
-    { path: 'treinos', component: Treinos },
-    { path: 'treinos/novo', component: TreinoForm },
-    { path: 'treinos/:id', component: TreinoDetalhe },
-    { path: 'exercicios', component: Exercicios }
+    { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
+    { path: 'treinos', component: Treinos, canActivate: [authGuard] },
+    { path: 'treinos/novo', component: TreinoForm, canActivate: [authGuard] },
+    { path: 'treinos/:id', component: TreinoDetalhe, canActivate: [authGuard] },
+    { path: 'exercicios', component: Exercicios, canActivate: [authGuard] }
 
 ];
